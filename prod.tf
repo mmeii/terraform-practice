2e4df7ee03b896b048a48a54c095d9f6d9756ba9
mmeii <redmmeii@gmail.com>
2022-02-03 10:47:48 -0800

    Adopt default vpc and rename tf file to prod

--- prod.tf
+++ prod.tf
@@ -3,8 +3,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "mei-tf-course"
   acl	 = "private"
 }
+
+resource "aws_default_vpc" "default" {}
 
